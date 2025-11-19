9414d4d0d5a89f2f96cf6d8852331dd278a2e154
CoreDumpNotCrash <nazarkolepkov008@gmail.com>
2025-11-19 20:55:10 +0200

    ft: hello worl

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-console.log("Hello, World!");
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+const __dirname = import.meta.dirname;
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+  });
+  mainWindow.loadFile(path.join(__dirname, "template", "index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
